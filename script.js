89c8d7df8c820dae1cdf998f254402fd87eae57c
SpyrosSt_2 <spirostamatiou@hotmai.com>
2024-08-08 01:01:45 +0300

    Added a new img folder, created UI through script.js and also added  style.css
    Added three buttons for the player's choice with their corresponding event Listeners.
    Changed all the console logs to DOM methods displaying the score, and added an alert upon win/lose.

--- script.js
+++ script.js
@@ -3,74 +3,75 @@ let message = "Lets play Rock Paper Scissors! \nType your choice (it is case ins
 
 function getComputerChoice() {
     let computerSelection = "";
-    let random = Math.floor(Math.random() * 3);
+    let rand = Math.floor(Math.random() * 3);
     if (rand === 0) {
         computerSelection = "rock";
-    } else if (random === 1) {
+    } else if (rand === 1) {
         computerSelection = "paper";
-    } else if (random === 2) {
+    } else if (rand === 2) {
         computerSelection = "scissors";
     }
     
     return computerSelection;
 }
 
-const arr = ["rock", "paper", "scissors", null];
+let humanScore = 0;
+let computerScore = 0;
+function playRound(humanChoice,computerChoice) {
+    const runningScore = document.querySelector(".running-score");
+    //TIE
+    if (humanChoice === computerChoice) {
+        const announcement = document.createElement("p");
+        announcement.textContent = `Its a tie! SCORE:  ${humanScore} - ${computerScore}`;
+        runningScore.appendChild(announcement);
+    }
+    //WIN
+    else if((humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")) {
+                humanScore++;
+                const announcement = document.createElement("p");
+                announcement.textContent = `You win! ${humanChoice} 
+                                            beats  ${computerChoice}
+                                            SCORE:  ${humanScore} - ${computerScore}`;
+                runningScore.appendChild(announcement);
+            } else {
+                computerScore++;
+                const announcement = document.createElement("p");
+                announcement.textContent = `You lose! ${computerChoice} 
+                                            beats  ${humanChoice}
+                                            SCORE:  ${humanScore} - ${computerScore}`;
+                runningScore.appendChild(announcement);
+            }
 
-function getHumanChoice() {
-    let humanSelection = "";
-    while (!arr.includes(humanSelection) ) {
-        humanSelection = prompt(message);
-        if (humanSelection !== null) {
-            humanSelection = humanSelection.toLowerCase();
+    setTimeout(() => {
+        if(humanScore === 5) {
+            alert('YOU WIN! PLAY AGAIN?');
+            runningScore.innerHTML = "";
+            humanScore = 0;
+            computerScore = 0;
+        } else if (computerScore === 5) {
+            alert('YOU LOSE! PLAY AGAIN?');
+            runningScore.innerHTML = "";
+            humanScore = 0;
+            computerScore = 0;
         }
-    }
-    if(humanSelection === null) {
-        console.log("You pressed Cancel. Game will stop now. You can reload to play again..")
-    }
-    return humanSelection;
-    
+    }, 100);
 }
 
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+let playerSelection;
+const buttons = document.querySelectorAll("button");
 
-    function playRound(humanChoice,computerChoice) {
-        //Cancel
-        if (humanChoice === null) {
-            return -1;
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (button.id === "rock-button") {
+            playerSelection = "rock";
+        } else if (button.id === "paper-button") {
+            playerSelection = "paper";
+        } else if (button.id === "scissors-button") {
+            playerSelection = "scissors";
         }
-        //TIE
-        if (humanChoice === computerChoice) {
-            console.log(`Its a tie!`);
-        }
-        //WIN
-        else if((humanChoice === "rock" && computerChoice === "scissors") ||
-                (humanChoice === "paper" && computerChoice === "rock") ||
-                (humanChoice === "scissors" && computerChoice === "paper")) {
-                    humanScore++;
-                    console.log(`You win! ${humanChoice} beats  ${computerChoice}`);
-                } else {
-                    computerScore++;
-                    console.log(`You lose! ${computerChoice} beats  ${humanChoice}`);
-                }
-    
-    }
-    for (let i = 0; i<5; i++) {
-        let check_cancel = playRound(getHumanChoice(), getComputerChoice());
-        if (check_cancel === -1)
-            return 0;
-        console.log(`SCORE: ${humanScore} - ${computerScore}`)
-    }
-    if(humanScore > computerScore) {
-        console.log("YOU WON!");
-    } else if (humanScore < computerScore){
-        console.log("YOU LOST!");
-    } else {
-        console.log("TIE!");
-    }
-}
-
-playGame();
+        playRound(playerSelection,getComputerChoice());
+    });
+});
